1b4cc4b19190250aa877f34c7879709250d0c8c4
snatu1996 <shubham.natu@cognizant.com>
2020-05-27 15:17:20 +0530

    Creating XML & XSD for request & response

---
+++ src/main/java/com/shubham/spring/webservices/springwebservicesdemo/SpringWebservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.spring.webservices.springwebservicesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebservicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebservicesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/spring/webservices/springwebservicesdemo/SpringWebservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.spring.webservices.springwebservicesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
